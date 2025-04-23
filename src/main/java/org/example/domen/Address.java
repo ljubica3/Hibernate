b103cd6c2d47352bd8e55e5f7457ae37befa81a0
ljubica.vlahovic <vlahovic.ljubica5@gmail.com>
2025-04-23 14:04:07 +0200

    domen i import

--- src/main/java/org/example/domen/Address.java
+++ src/main/java/org/example/domen/Address.java
@@ -1,23 +1,13 @@
 package org.example.domen;
 
-import javax.persistence.Embeddable;
+import jakarta.persistence.Embeddable;
 
 @Embeddable
 public class Address {
 
     private String street;
-    private String zip_code;
+    private String zipcode;
     private String city;
-
-    public Address(String street, String city, String zipCode) {
-        this.street = street;
-        this.city = city;
-        this.zip_code= zip_code;
-    }
-
-    public Address() {
-
-    }
 
     public String getStreet() {
         return street;
@@ -27,12 +17,12 @@ public class Address {
         this.street = street;
     }
 
-    public String getZip_code() {
-        return zip_code;
+    public String getZipcode() {
+        return zipcode;
     }
 
-    public void setZip_code(String zip_code) {
-        this.zip_code = zip_code;
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
     }
 
     public String getCity() {
@@ -42,14 +32,4 @@ public class Address {
     public void setCity(String city) {
         this.city = city;
     }
-
-    @Override
-    public String toString() {
-        return "Address{" +
-                "street='" + street + '\'' +
-                ", zip_code='" + zip_code + '\'' +
-                ", city='" + city + '\'' +
-                '}';
-    }
 }
-
